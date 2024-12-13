9658536fff5392fcc74b8eba0ccb1d57393c7feb
Ismael Firmiano <ismaelfirmiano@id.uff.br>
2024-12-13 16:11:41 -0300

    Criação dos registradores

---
+++ mic-1/src/VD/Registrador.java
@@ -0,0 +1,11 @@
+package VD;
+
+public class Registrador {
+    int valor;
+    String nome;
+
+    Registrador(int valor, String nome) {
+        this.valor = valor;
+        this.nome = nome;
+    }
+}
